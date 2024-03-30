390902698ad7e737b7b188e7dfce6a52ab6baaeb
M. D. KOREN <109611097+matheusmeteoro@users.noreply.github.com>
2024-03-30 21:55:54 +0000

    progress

--- isingmodel.py
+++ isingmodel.py
@@ -1,5 +1,23 @@
+#Importar las bibliotecas
+
+import numpy as np
+import matplotlib.pyplot as plt
+import numba
+from numba import njit
+from scipy.ndimage import convolve, generate_binary_structure
+
 #Red cuadrada 50x50
 N = 50
 
 #Generar algunas cuadrículas iniciales aleatorias de espines
+init_random = np.random.random((N,N))
+lattice_n = np.zeros((N,N))
+lattice_n[init_random>=0.75] = 1
+lattice_n[init_random<0.75] = -1
 
+init_random = np.random.random((N,N))
+lattice_p = np.zeros((N,N))
+lattice_p[init_random>0.25] = 1
+lattice_p[init_random<0.25] = -1
+
+plt.imshow(lattice_p)
